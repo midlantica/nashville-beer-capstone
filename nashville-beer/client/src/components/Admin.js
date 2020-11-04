7903bd1412e29a4e50551a32a84813f212fbd3d6
drewharper <druharper@gmail.com>
2020-11-03 19:07:59 -0600

    admin

--- nashville-beer/client/src/components/Admin.js
+++ nashville-beer/client/src/components/Admin.js
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect, useState } from "react";
 import Beers from "./Beers";
 import Brewery from "./Brewery";
+import FormEditBrewery from "./FormEditBrewery";
 import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
 import { NavLink as RRNavLink, Link, useParams, useHistory } from "react-router-dom";
 //import { UserProfileContext } from "../providers/UserProfileProvider";
@@ -58,93 +59,40 @@ export default function Admin() {
           <h4>Admin</h4>
           <div className="dh-form-grid">
 
-            {/* SIDEBAR */}
             <div className="sidebar">
+              <div className="dh-brewAdminHead flexRow just-space-between my-2 pr-2">
+                <h5>Breweries</h5>
+                <div className="dh-add"><Link to="./">+</Link></div>
+              </div>
 
-                <h6 className="my-2">Breweries</h6>
-
-                <ul className="dh-admin-breweries">
-                  <li className="dh-add">
-                    <Link to="./">Add Brewery</Link></li>
-                    {breweries.map((i) =>
-                    <li key={i.id}>
-                      <details>
-                        <summary>{i.title}
-                          <button id={i.id}
-                                  className="dh-btn-delete"
-                                  onClick={() => deleteThisBrewery(i.id)}>-
-                          </button>
-                        </summary>
-                        <ul className="dh-admin-beers">
-                          {i.beers.map(beer =>
-                            <Link key={beer.id} to={beer.id}>
-                              <li>{beer.name}</li>
-                            </Link>
-                          )}
-                              <li className="dh-add"><Link to="AddBeer">Add Beer</Link></li>
-                        </ul>
-                      </details>
-                    </li>
-                  )}
-                </ul>
+              <ul className="dh-admin-breweries">
+                  {breweries.map((i) =>
+                  <li key={i.id}>
+                    <details>
+                      <summary>{i.title}
+                        <button id={i.id}
+                                className="dh-btn-delete"
+                                onClick={() => deleteThisBrewery(i.id)}>-
+                        </button>
+                      </summary>
+                      <ul className="dh-admin-beers">
+                        {i.beers.map(beer =>
+                          <Link key={beer.id} to={beer.id}>
+                            <li>{beer.name}</li>
+                          </Link>
+                        )}
+                      </ul>
+                      <div className="dh-add-beer"><Link to="AddBeer">Add New Beer</Link></div>
+                    </details>
+                  </li>
+                )}
+              </ul>
 
             </div>
 
             {/* CONTENT FORM */}
             <div className="content">
-              <h4>Brewery</h4>
-              <Form style={{ maxWidth: "600px", minWidth: "300px"}}>
-
-                <FormGroup>
-                  <Label for="title">Title</Label>
-                  <Input type="text"
-                    onChange={handleFieldChange}
-                    name="title"
-                    id="title"
-                    placeholder="title" />
-                </FormGroup>
-
-                <FormGroup>
-                  <Label for="address">Address</Label>
-                  <Input type="address"
-                    onChange={handleFieldChange}
-                    name="address"
-                    id="address"
-                    placeholder="address" />
-                </FormGroup>
-
-                <FormGroup>
-                  <Label for="website">Website</Label>
-                  <Input type="website"
-                    onChange={handleFieldChange}
-                    name="website"
-                    id="website"
-                    placeholder="website" />
-                </FormGroup>
-
-                <FormGroup>
-                  <Label for="established">Established</Label>
-                  <Input type="established"
-                    onChange={handleFieldChange}
-                    name="established"
-                    id="established"
-                    placeholder="established" />
-                </FormGroup>
-
-                <FormGroup>
-                  <Label for="imageUrl">Image Url</Label>
-                  <Input type="imageUrl"
-                    onChange={handleFieldChange}
-                    name="imageUrl"
-                    id="imageUrl"
-                    placeholder="imageUrl" />
-                </FormGroup>
-
-                <div className="d-flex">
-                  <Button color="secondary" size="sm">Cancel</Button>
-                  <Button color="primary ml-3" size="sm" onClick={makeNewBrewery}>Add New Brewery</Button>
-                </div>
-              </Form>
+              <FormEditBrewery/>
             </div>
         </div>
       </section>
